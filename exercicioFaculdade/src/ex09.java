5d202cfd39391ba7ac006c2e1dd6154a458dc909
Gustavo Paulo <gustavopaulodejesus797@gmail.com>
2026-03-04 09:52:44 -0300

    Delete exercicioFaculdade directory

--- exercicioFaculdade/src/ex09.java
+++
@@ -1,7 +0,0 @@
-public class ex09 {
-    public static void main(String[]args) {
-        double lado = 350.00;
-        double area = lado*lado;
-        System.out.printf("A áres é: %.2f",area);
-    }
-}
